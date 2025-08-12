8191b43d816405003f080288d70ecfae99e8d1c7
Anton Kovsharov <antonkov@google.com>
2025-08-12 18:28:11 +0000

    Update to Lean 4.21

--- src/lean4/lakefile.lean
+++ src/lean4/lakefile.lean
@@ -6,8 +6,11 @@ package «clever» where
   leanOptions := #[
     ⟨`autoImplicit, false⟩
   ]
-require mathlib from git "https://github.com/leanprover-community/mathlib4" @ "v4.15.0"
+require mathlib from git "https://github.com/leanprover-community/mathlib4" @ "v4.21.0"
 
 @[default_target]
-lean_lib «clever» where
+lean_lib common where
+  globs := #[.submodules `Imports]
+
+lean_lib everything where
   globs := #[.submodules `Imports, .submodules `human_eval, .submodules `sample_examples]
